9a9d04a2b8fa71c3767ebd3e45f27e07630e10b8
zaljifri <zainab.aljifri@gmail.com>
2022-12-05 20:56:57 +0300

    Edit settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'django-insecure-4yd*q89w38y$^^9orr(a%md_qke(%@nr+!ca81h6f=!rb48#9s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  [
-    '.vercel.app'
-    ]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -81,10 +79,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
